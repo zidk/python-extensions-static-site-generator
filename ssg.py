54876069b9a651c8dcca30f8f92425b39e5bec71
Tom Bell <tjbell@gmail.com>
2020-03-25 13:09:40 -0600

    initial commit

---
+++ ssg.py
@@ -0,0 +1,19 @@
+import typer
+
+from ssg.site import Site
+import ssg.parsers
+
+def main(source="content", dest="dist"):
+    config = {
+        "source": source,
+        "dest": dest,
+        "parsers": [
+            ssg.parsers.ResourceParser(),
+            ssg.parsers.MarkdownParser(),
+            ssg.parsers.ReStructuredTextParser(),
+        ],
+    }
+
+    Site(**config).build()
+
+typer.run(main)
---
+++ ssg/parsers.py
@@ -0,0 +1,57 @@
+import shutil
+import sys
+
+from typing import List
+from pathlib import Path
+
+from docutils.core import publish_parts
+from markdown import markdown
+from ssg.content import Content
+
+
+class Parser:
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+
+    def read(self, path):
+        with open(path, "r") as file:
+            return file.read()
+
+    def write(self, path, dest, content, ext=".html"):
+        full_path = dest / path.with_suffix(ext).name
+        with open(full_path, "w") as file:
+            file.write(content)
+
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+
+class ResourceParser(Parser):
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path, source, dest):
+        self.copy(path, source, dest)
+
+
+class MarkdownParser(Parser):
+    extensions = [".md", ".markdown"]
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+
+
+class ReStructuredTextParser(Parser):
+    extensions = [".rst"]
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
