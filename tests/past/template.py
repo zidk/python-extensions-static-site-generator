cd20e6464a4f4aef1c7f6ba954f064a713d23910
Tom Bell <tjbell@gmail.com>
2020-04-08 13:37:11 -0600

    test: combine parse and template

--- tests/past/template.py
+++ tests/past/template.py
@@ -1,6 +1,5 @@
 # https://github.com/takluyver/astcheck
 # https://github.com/takluyver/astsearch
-
 """
 Copyright (c) 2014 Thomas Kluyver
 
@@ -28,13 +27,155 @@ import os.path
 import warnings
 import ast
 
-
-from .parse import convert_node
-
-
 class Template(object):
     def __init__(self, pattern):
         self.pattern = TemplateTransformer.load(pattern)
+
+    # https://github.com/wildptr/sexp_of_python
+    # https://docs.python.org/3/library/ast.html#abstract-grammar
+    schema = {
+        # mod
+        ast.Module: ["body"],
+        ast.Interactive: ["body"],
+        ast.Expression: ["body"],
+        ast.Suite: ["body"],
+        # stmt
+        ast.FunctionDef: ["name", "args", "body", "decorator_list", "returns"],
+        ast.AsyncFunctionDef: ["name", "args", "body", "decorator_list", "returns"],
+        ast.ClassDef: ["name", "bases", "keywords", "body", "decorator_list"],
+        ast.Return: ["value"],
+        ast.Delete: ["targets"],
+        ast.Assign: ["targets", "value"],
+        ast.AugAssign: ["target", "op", "value"],
+        ast.AnnAssign: ["target", "annotation", "value", "simple"],
+        ast.For: ["target", "iter", "body", "orelse"],
+        ast.AsyncFor: ["target", "iter", "body", "orelse"],
+        ast.While: ["test", "body", "orelse"],
+        ast.If: ["test", "body", "orelse"],
+        ast.With: ["items", "body"],
+        ast.AsyncWith: ["items", "body"],
+        ast.Raise: ["exc", "cause"],
+        ast.Try: ["body", "handlers", "orelse", "finalbody"],
+        ast.Assert: ["test", "msg"],
+        ast.Import: ["names"],
+        ast.ImportFrom: ["module", "names", "level"],
+        ast.Global: ["names"],
+        ast.Nonlocal: ["names"],
+        ast.Expr: ["value"],
+        ast.Pass: [],
+        ast.Break: [],
+        ast.Continue: [],
+        # expr
+        ast.BoolOp: ["op", "values"],
+        ast.BinOp: ["left", "op", "right"],
+        ast.UnaryOp: ["op", "operand"],
+        ast.Lambda: ["args", "body"],
+        ast.IfExp: ["test", "body", "orelse"],
+        ast.Dict: ["keys", "values"],
+        ast.Set: ["elts"],
+        ast.ListComp: ["elt", "generators"],
+        ast.SetComp: ["elt", "generators"],
+        ast.DictComp: ["key", "value", "generators"],
+        ast.GeneratorExp: ["elt", "generators"],
+        ast.Await: ["value"],
+        ast.Yield: ["value"],
+        ast.YieldFrom: ["value"],
+        ast.Compare: ["left", "ops", "comparators"],
+        ast.Call: ["func", "args", "keywords"],
+        ast.Num: ["n"],
+        ast.Str: ["s"],
+        ast.FormattedValue: ["value", "conversion", "format_spec"],
+        ast.JoinedStr: ["values"],
+        ast.Bytes: ["s"],
+        ast.NameConstant: ["value"],
+        ast.Ellipsis: [],
+        ast.Constant: ["value"],
+        ast.Attribute: ["value", "attr"],
+        ast.Subscript: ["value", "slice"],
+        ast.Starred: ["value"],
+        ast.Name: ["id"],
+        ast.List: ["elts"],
+        ast.Tuple: ["elts"],
+        # expr_context
+        ast.Load: [],
+        ast.Store: [],
+        ast.Del: [],
+        ast.AugLoad: [],
+        ast.AugStore: [],
+        ast.Param: [],
+        # slice
+        ast.Slice: ["lower", "upper", "step"],
+        ast.ExtSlice: ["dims"],
+        ast.Index: ["value"],
+        # boolop
+        ast.And: [],
+        ast.Or: [],
+        # operator
+        ast.Sub: [],
+        ast.Mult: [],
+        ast.MatMult: [],
+        ast.Div: [],
+        ast.Mod: [],
+        ast.Pow: [],
+        ast.LShift: [],
+        ast.RShift: [],
+        ast.BitOr: [],
+        ast.BitXor: [],
+        ast.BitAnd: [],
+        ast.FloorDiv: [],
+        # unaryop
+        ast.Invert: [],
+        ast.Not: [],
+        ast.UAdd: [],
+        ast.USub: [],
+        # cmpop
+        ast.Eq: [],
+        ast.NotEq: [],
+        ast.Lt: [],
+        ast.LtE: [],
+        ast.Gt: [],
+        ast.GtE: [],
+        ast.Is: [],
+        ast.IsNot: [],
+        ast.In: [],
+        ast.NotIn: [],
+        # comprehension
+        ast.comprehension: ["target", "iter", "ifs", "is_async"],
+        # excepthandler
+        ast.ExceptHandler: ["type", "name", "body"],
+        # arguments
+        ast.arguments: ["args", "vararg", "kwonlyargs", "kw_defaults", "kwarg", "defaults"],
+        # arg
+        ast.arg: ["arg", "annotation"],
+        # keyword
+        ast.keyword: ["arg", "value"],
+        # alias
+        ast.alias: ["name", "asname"],
+        # withitem
+        ast.withitem: ["context_expr", "optional_vars"],
+    }
+
+    def convert_node(self, node):
+        t = type(node)
+
+        if t is str or t is int:
+            return node
+
+        if t is list:
+            return [self.convert_node(child) for child in node]
+
+        if node is None:
+            return "nil"
+
+        tname = t.__qualname__
+        d = {"type": tname}
+
+        if t not in self.schema:
+            return f"#<{tname}>"
+
+        for name in self.schema[t]:
+            d[name] = self.convert_node(getattr(node, name))
+        return d
 
     def query(self, code):
         if isinstance(code, str):
@@ -44,7 +185,7 @@ class Template(object):
         nodetype = type(self.pattern)
         for node in ast.walk(tree):
             if isinstance(node, nodetype) and is_ast_equal(node, self.pattern):
-                yield convert_node(node)
+                yield self.convert_node(node)
 
     def query_file(self, filename):
         if isinstance(filename, str):
--- tests/test_past.py
+++ tests/test_past.py
@@ -1,40 +1,8 @@
-import ast
-import pmast
 import pytest
-import json
 
-# from pmast import ast_type, Pattern, PatternDispatch
-# from box import Box
-from astpretty import pprint
+
 from .past.template import Template
 from pathlib import Path
-
-
-# pm = PatternDispatch()
-# container = Box({"functions": {}, "classes": {}})
-# with open(Path.cwd() / "ssg" / "content.py", "r") as file:
-#     source = file.read()
-# tree = ast.parse(source)
-#
-#
-# @pm("ClassDef")
-# def on_class(data, cd):
-#     data.classes[cd.name] = cd
-
-
-# @pm("FunctionDef")
-# def on_function(data, fd):
-#     pprint(fd)
-    # data.functions[fd.name] = {}
-    # data.functions[fd.name].args = [arg.arg for arg in fd.args.args]
-    # data.functions[fd.name].body = fd.body
-
-
-# @pytest.mark.test_pmast_example
-# def test_pmast_example(parse):
-#     data = pm.dispatch(tree, container)
-#     assert False
-
 
 
 @pytest.mark.test_past_example
