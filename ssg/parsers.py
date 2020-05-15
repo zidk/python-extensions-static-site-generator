8fddb4ea943b733b26479495e6bfe950efacaa09
Tom Bell <tjbell@gmail.com>
2020-05-15 15:29:55 -0600

    module3-solution

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -8,6 +8,8 @@ from docutils.core import publish_parts
 from markdown import markdown
 from ssg.content import Content
 
+from ssg import hooks
+
 
 class Parser:
     base_ext = ".html"
@@ -45,7 +47,8 @@ class MarkdownParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(path, dest, html)
+        filtered = hooks.filter("generate_menu", html, self.base_ext)
+        self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
@@ -57,7 +60,8 @@ class ReStructuredTextParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
-        self.write(path, dest, html["html_body"])
+        filtered = hooks.filter("generate_menu", html["html_body"], self.base_ext)
+        self.write(path, dest, filtered)
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
--- ssg/site.py
+++ ssg/site.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-from ssg import extensions
+from ssg import extensions, hooks
 
 
 class Site:
@@ -30,6 +30,7 @@ class Site:
 
     def build(self):
         extensions.load_bundled()
+        hooks.event("collect_files", self.source, self.parsers)
         self.dest.mkdir(parents=True, exist_ok=True)
         for path in self.source.rglob("*"):
             if path.is_dir():
