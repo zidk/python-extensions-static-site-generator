a250c738b32a9171fda55de62e8f15dba7c9a582
zidesk <zidesk@gmail.com>
2020-11-07 18:55:33 +0100

    fix insert params order and rglob pattern

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -4,13 +4,13 @@ from pathlib import Path
 
 
 def load_module(directory, name):
-    sys.path.insert(directory, 0)
+    sys.path.insert(0, directory)
     importlib.import_module(name)
     sys.path.pop(0)
 
 
 def load_directory(directory):
-    for path in directory.rglob('.py'):
+    for path in directory.rglob('*.py'):
         load_module(directory.as_posix(), path.stem)
 
 
